731598f69d03370323686b784f1ba383560de3d0
Tom Collingwood <tom.collingwood@stfc.ac.uk>
2021-06-28 19:08:23 +0100

    delete try-repo script

--- tst.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pre-commit try-repo ./ check-missing-requirements --all-files
-pre-commit try-repo ./ check-mypy-import-errors --all-files
-pre-commit try-repo ./ check-pylint-import-errors --all-files
